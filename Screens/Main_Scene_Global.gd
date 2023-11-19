38da1a4fc99971f139fef29316405da7b1d1fd61
BERSERKER\Hunter <Hunter@raglandonline.com>
2023-11-18 19:35:18 -0500

    Connected buttons to script

--- Screens/Main_Scene_Global.gd
+++ Screens/Main_Scene_Global.gd
@@ -1,2 +1,20 @@
 extends Node2D
 
+
+
+
+
+func _on_top_left_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_top_right_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_bottom_right_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_bottom_left_button_pressed():
+	pass # Replace with function body.
